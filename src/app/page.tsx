053c9bbafa8fec6944c1c0690d82d85ded8de491
Van Thao <vanthao.work@gmail.com>
2023-06-21 23:16:46 +0700

    Set up inital project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,54 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
 
-export default function Home() {
+import { decrement, increment, reset } from '@/redux/features/counterSlice';
+import { useAppDispatch, useAppSelector } from '@/redux/hooks';
+import { useGetUsersQuery } from '@/redux/services/userApi';
+import Image from 'next/image';
+import React from 'react';
+
+const Home: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const count = useAppSelector((state) => state.counterReducer.value);
+  const { isFetching, isLoading, data, error } = useGetUsersQuery(null);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main style={{ maxWidth: 1200, marginInline: 'auto', padding: 20 }}>
+      <div style={{ marginBottom: '4rem', textAlign: 'center' }}>
+        <h4 style={{ marginBottom: 16 }}>{count}</h4>
+        <button onClick={() => dispatch(increment())}>increment</button>
+        <button onClick={() => dispatch(decrement())} style={{ marginInline: 16 }}>
+          decrement
+        </button>
+        <button onClick={() => dispatch(reset())}>reset</button>
+      </div>
+
+      {error != null ? (
+        <p>Oh no, there was an error</p>
+      ) : isLoading || isFetching ? (
+        <p>Loading...</p>
+      ) : data != null ? (
+        <div
+          style={{
+            display: 'grid',
+            gridTemplateColumns: '1fr 1fr 1fr 1fr',
+            gap: 20,
+          }}
+        >
+          {data.map((user) => (
+            <div key={user.id} style={{ border: '1px solid #ccc', textAlign: 'center' }}>
+              <Image
+                src={`https://robohash.org/${user.id}?set=set2&size=180x180`}
+                alt={user.name}
+                width={180}
+                height={180}
+              />
+              <h3>{user.name}</h3>
+            </div>
+          ))}
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      ) : null}
     </main>
-  )
-}
+  );
+};
+
+export default Home;
